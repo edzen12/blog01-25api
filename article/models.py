3e480e20ca7509a8128c106da7cdd3737781780e
edzen12 <oichiev.edzen@gmail.com>
2026-04-29 11:21:15 +0600

    add category and media

--- article/models.py
+++ article/models.py
@@ -1,8 +1,30 @@
 from django.db import models
+from django.utils.text import slugify
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+            counter = 1
+            orginal_slug = self.slug
+            while Category.objects.filter(slug=self.slug).exists():
+                self.slug = f"{orginal_slug}-{counter}"
+                counter+=1
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
+    image = models.ImageField(
+        upload_to='posts/', null=True, blank=True
+    )
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
